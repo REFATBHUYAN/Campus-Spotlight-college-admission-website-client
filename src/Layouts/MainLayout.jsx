b071a5db2bd3802bae5e43dfd0248bc4b26b8d81
REFATBHUYAN <refatbubt@gmail.com>
2023-07-24 18:36:05 +0600

    footer added

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React, { Children } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 
 const MainLayout = () => {
     return (
         <div className='max-w-full mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
